4c67b291b6fb294dc4b6e27165259cd6255448b8
Matt Tierney <MRTierney@gmail.com>
2011-02-23 10:03:59 -0500

    getting there

--- src/util.py
+++ src/util.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import subprocess
 def execute(cmd):
     subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
                      stderr=subprocess.PIPE).communicate()

c63131f569d36ca77d5270c12a7c3f12f9d4aec9
Russell Power <power@cs.nyu.edu>
2011-03-03 16:33:14 -0500

    Launch browser when starting configuration.
    Try to fill in form elements with guesses based on system information.

--- src/web-sandbox/server.py
+++ src/web-sandbox/server.py
@@ -2,12 +2,18 @@
 
 import BaseHTTPServer
 import urllib, urlparse, urllib2
-import json
-import os
-from os.path import basename, join, isfile, isdir, expanduser, split
-import cgitb
-import mimetypes
+import cgitb, mimetypes
 import ConfigParser
+import json
+import getpass, os, platform, sys, socket, time
+
+from string import Template
+
+from os.path import basename, join, isfile, isdir, expanduser, split
+shutdown_time = -1
+
+class HTMLTemplate(Template):
+  delimiter = '%'  
 
 class ConfigHandler(BaseHTTPServer.BaseHTTPRequestHandler):
   def write(self, ctype, data, code=200):
@@ -16,6 +22,11 @@ class ConfigHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     self.end_headers()
     self.wfile.write(data)
   
+  def send_content(self, path):
+    mtype = mimetypes.guess_type('file://' + path)
+    self.write(mtype[0], open('res' + path).read())
+  
+
   def tree_json(self, path, q):
     d = expanduser(q['path'][0])
     children = []
@@ -29,12 +40,21 @@ class ConfigHandler(BaseHTTPServer.BaseHTTPRequestHandler):
       out = [dict(data = split(d)[1], attr = {"path" : d}, state = 'open', children = children)]      
     
     self.write('text/javascript', json.dumps(out, indent=2))
+    
+  def tmpl(self, path, q):
+    path = q['template'][0]
+    mtype = mimetypes.guess_type('file://' + path)
+    ctmpl = HTMLTemplate(open('res' + path).read())
+    
+    content = ctmpl.substitute(dict(USER_EMAIL = '%s@%s' % (getpass.getuser(), socket.gethostname()),
+                                    COMPUTER_NAME = '%s' % platform.node()))
+    
+    self.write(mtype[0], content)
   
   def configure(self, path, q):
     c = ConfigParser.ConfigParser()
     c.add_section('sdb')
-    for key in ['firstName',
-                 'lastName',
+    for key in ['name',
                  'userEmailAddress',
                  'userPassword',
                  'awsAccessKey',
@@ -44,18 +64,23 @@ class ConfigHandler(BaseHTTPServer.BaseHTTPRequestHandler):
       c.set('sdb', key, q[key][0])
     
     admin_dir = os.path.join(os.environ["HOME"], ".safedepositbox")
-    os.makedirs(admin_dir)
+    try:
+      os.makedirs(admin_dir)
+    except OSError, e:
+      import errno
+      if e[0] != errno.EEXIST:
+        raise
     c.write(open(os.path.join(admin_dir, 'safedepositbox.conf'), 'w'))
     
-    self.send_content('success.html')
+    self.send_content('/success.html')
     
-  def send_content(self):
-    mtype = mimetypes.guess_type('file://' + path)
-    self.write(mtype[0], open('res' + path).read())
-  
+    # let this request out, then shutdown
+    global shutdown_time 
+    shutdown_time = time.time() + 3
+    
   def do_GET(self):
     if self.path == '/':
-      self.path = '/configure.html'
+      self.path = '/tmpl?template=/configure.html'
 
     url = urlparse.urlsplit(self.path, scheme='http')
     path = url.path
@@ -79,9 +104,26 @@ class ConfigHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             
 
 def configure():
-  httpd = BaseHTTPServer.HTTPServer(('localhost', 8080), ConfigHandler)
-  httpd.serve_forever(poll_interval=0.1)
+  def start_httpd():
+    global httpd
+    httpd = BaseHTTPServer.HTTPServer(('localhost', 8080), ConfigHandler)
+    httpd.timeout = 1
+    while 1:
+      httpd.handle_request()
+      if shutdown_time > 0:
+        print 'Shutting down in: %.2f' % (shutdown_time - time.time())
+        if time.time() > shutdown_time: 
+          break
+     
+  import threading
+  httpd_thread = threading.Thread(target = start_httpd)
+  httpd_thread.setDaemon(True)  
+  httpd_thread.start()
   
+  import webbrowser
+  webbrowser.open_new('http://localhost:8080')
+   
+  httpd_thread.join()
   
 if __name__ == '__main__':
   configure()

c62698b697c3a4364bbeb6ee840f7f0567fd494a
Russell Power <power@cs.nyu.edu>
2011-03-04 22:50:57 -0500

    Add M2Crypto support library.

---
+++ src/extern/__init__.py
@@ -0,0 +1,6 @@
+'''Adjust sys.path to point to the proper extern helper libraries.'''
+
+import os, sys
+BASE = os.path.dirname(__file__)
+sys.path = sys.path + [os.path.join(BASE, p) for p in 
+                       ['M2Crypto-0.21.1/build/lib.linux-x86_64-2.7/']]

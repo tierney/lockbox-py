5986456f284f55241b5a0147e7c796727717d188
tierney <devnull@localhost>
2011-05-15 12:24:17 -0400

    modified controls

--- src/sandboxes/python/CommandLineControls.py
+++ src/sandboxes/python/CommandLineControls.py
@@ -7,17 +7,80 @@ import sys
 
 prompt_string = "Lockbox"
 
-class MyCommands(cmd.Cmd):
+class LockboxCommand(cmd.Cmd):
     def __init__(self):
         cmd.Cmd.__init__(self)
-        self.prompt = prompt_string+'> '
+        self.prompt = prompt_string + '> '
 
-    def do_hello(self, arg):
-        print "hello again", arg, "!"
+    # editors
+    def do_addeditor(self):
+        pass
+    
+    def do_deleditor(self):
+        pass
+    
+    def do_modeditor(self):
+        pass
 
-    def help_hello(self):
-        print "syntax: hello [message]",
-        print "-- prints a hello message"
+    def do_searcheditors(self):
+        # see address book
+        pass
+
+    # keys
+    def do_genkey(self):
+        pass
+
+    def do_addkey(self):
+        """
+        TODO username + location == key
+        """
+        pass
+    
+    def do_modkey(self):
+        # change username or location associated with key
+        pass
+    
+    def do_delkey(self):
+        pass
+    
+    def do_lskeys(self):
+        pass
+    
+    # Adjust association between editor and file
+    def do_addeditortofile(self):
+        # username
+        pass
+
+    def do_deleditorfromfile(self):
+        pass
+
+    def do_lsfileeditors(self, filename):
+        pass
+    
+    # file manipulation
+    def do_lsfiles(self):
+        pass
+
+    # top directory manipulation
+    def do_lstopdirs(self):
+        pass
+    
+    def do_addtopdir(self):
+        pass
+    
+    def do_rmtopdir(self):
+        pass
+    
+    # web service setup
+    def do_cost(self):
+        pass
+
+    # Lockbox System testing
+    def do_upgradecheck(self):
+        pass
+
+    def do_upgradestart(self):
+        pass
 
     def do_quit(self, arg):
         sys.exit(1)
@@ -29,9 +92,9 @@ class MyCommands(cmd.Cmd):
     # shortcuts
     do_q = do_quit
 
-    def do_add(self,s):
+    def do_add(self, s):
         l = s.split()
-        if len(l)!=2:
+        if len(l) != 2:
            print "*** invalid number of arguments"
            return
         try:
@@ -39,10 +102,10 @@ class MyCommands(cmd.Cmd):
         except ValueError:
            print "*** arguments should be numbers"
            return
-        print l[0]+l[1]
+        print l[0] + l[1]
 
 def initialize_logging():
-    FORMAT="%(asctime)-15s %(levelname)-10s %(module)-10s %(lineno)-3d %(message)s"
+    FORMAT = "%(asctime)-15s %(levelname)-10s %(module)-10s %(lineno)-3d %(message)s"
     logging.basicConfig(filename="test.log", format=FORMAT)
 
 def main():
@@ -54,5 +117,5 @@ def main():
     # cli = CLI()
     cli.cmdloop()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()

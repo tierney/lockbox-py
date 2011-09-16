b2289c1f0537d41a74232cae10755f78b680ee13
Matt Tierney <tierney@cs.nyu.edu>
2011-09-15 22:02:05 -0400

    SimpleDB hacking.

--- src/lockbox/simpledb.py
+++ src/lockbox/simpledb.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
+"""SimpleDB methods for the Lockbox project."""
 
-import boto
+__copyright__ = 'Matt Tierney'
+__license__ = 'GPLv3'
+__author__ = 'tierney@cs.nyu.edu (Matt Tierney)'
+
 import os
+import boto
+import logging
 from uuid import uuid4
 from time import time as epoch_time
 from hashlib import sha1
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+_TIMEOUT_SECONDS = 10
 
 def _get_random_uuid():
   return uuid4()
@@ -55,27 +67,31 @@ def domain_insert(domain, data):
 
 
 def _select_object_locks_query(lock_domain, object_id):
-  return "select * from %s where `name` like '%s%%'" % \
+  """
+  Return string query for locks whose names start with the name of the object.
+  """
+  return "select * from %s where `lock_name` like '%s%%'" % \
          (lock_domain, object_id)
 
+def release_domain_object_lock(lock_domain, lock):
+  """Deletes the lock for the object."""
+  lock_domain.delete_item(lock)
 
-def lock_domain_object(lock_domain, object_id):
+  
+def acquire_domain_object_lock(lock_domain, object_id):
   """
   Returns:
     (bool success, lock)
     success: Indicates whether we won the bid to write the object.
     lock: Data about the lock we either found for this object or that we crated.
   """
+  logging.debug("testing")
   # Get a valid new lock name
   lock_id = _lock_name(object_id)
-  _ = lock_domain.get_item(lock_id, consistent_read=True)
-  if _:
-    # Found a lock of the exact same name. Since a lock already exists for this
-    # object on the cloud, we are going to wait to try again.
-    return False, _lock
 
-  query = _select_object_locks_query(domain_name_group_locks, object_id)
-  matching_locks = domain_group_locks.select(query, consistent_read=True)
+  # Check if we already have a lock for the object.
+  query = _select_object_locks_query(lock_domain.name, object_id)
+  matching_locks = lock_domain.select(query, consistent_read=True)
   try:
     _ = matching_locks.next()
     # Found a lock for the same object.
@@ -83,11 +99,42 @@ def lock_domain_object(lock_domain, object_id):
   except StopIteration:
     # Did not find any locks.
     pass
+  except boto.exception.SDBResponseError:
+    logging.info('Not quite what we were hoping for.')
+    pass
 
-  unconfirmed_lock = lock_domain.new_item(lock_id)
-  unconfirmed_lock['name'] = lock_id
-  unconfirmed_lock['user'] = 'tierney'
-  unconfirmed_lock['epoch_time'] = epoch_time()
+  # Set attributes for a new, unconfirmed lock.
+  unconfirmed_lock = lock_domain.new_item(unicode(lock_id))
+  unconfirmed_lock[unicode('lock_name')] = unicode(lock_id)
+  unconfirmed_lock[unicode('user')] = unicode('tierney')
+  unconfirmed_lock[unicode('epoch_time')] = unicode(epoch_time())
+  unconfirmed_lock.save()
+
+  # Check if another lock was taken at this time. If so, we release our lock.
+  query = _select_object_locks_query(lock_domain.name, object_id)
+  matching_locks = lock_domain.select(query, consistent_read=True)
+  for _lock in matching_locks:
+    # Finding a matching lock verifies that we can read the lock that we wrote.
+    if _lock == unconfirmed_lock:
+      continue
+
+    # Check if we should ignore the lock due to a timeout.
+    if (epoch_time() - float(_lock.epoch_time) > _TIMEOUT_SECONDS):
+      logging.info('Encountered (and ignoring) an old lock: %s.' % _lock)
+      continue
+    
+    # Concede. Delete our lock and return failure.
+    logging.info('Found a conflicting lock so we must release.\n' \
+                 ' found lock: %s.\n' \
+                 ' unconfirmed lock: %s.' %
+                 (_lock, unconfirmed_lock))
+    # Another lock found. Need to release ours.
+    lock_domain.delete_item(unconfirmed_lock)
+    return False, _lock
+
+  # Return success and the lock (we have confirmed it at this point though).
+  return True, unconfirmed_lock
+
 
 
 def main():
@@ -98,7 +145,7 @@ def main():
     (prev == NULL) ==> key represents a file checkpoint.
   """
 
-  conn = boto.connect_sdb(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+  conn = boto.connect_sdb() # AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
 
   domain_name_group = 'group0'
   domain_name_group_locks = 'group0_locks'
@@ -111,15 +158,12 @@ def main():
 
   object_id = _get_random_uuid()
 
-  lock_name = _lock_name(object_id)
-  locks[lock_name] = { 'name': lock_name,
-                       'user' : 'myusername',
-                       'epoch_time' : epoch_time() }
-  lock_name = _lock_name(object_id)
-  locks[lock_name] = { 'name': lock_name,
-                       'user' : 'myusername',
-                       'epoch_time' : epoch_time() }
-
+  success, lock = acquire_domain_object_lock(domain_group_locks, object_id)
+  if not success:
+    print 'We had a problem.'
+    print lock
+    return
+  
   object_name_0 = '%s-0' % object_id
   object_name_1 = '%s-1' % object_id
   data[object_name_0] = { 'sha1_0' : '',
@@ -131,15 +175,14 @@ def main():
 
 
   domain_insert(domain_group, data)
-  domain_insert(domain_group_locks, locks)
 
   print domain_group.get_item(object_name_0)
-  for entry in domain_group_locks.select("select * from %s where `name` like '%s%%'" %
+  for entry in domain_group_locks.select("select * from %s where `lock_name` like '%s%%'" %
                                          (domain_name_group_locks, object_id),
                                          consistent_read=True):
-    print "Lock entry:", entry
+    print 'Lock entry:', entry
 
-  print "Cleaning up."
+  print 'Cleaning up.'
   conn.delete_domain(domain_name_group)
   conn.delete_domain(domain_name_group_locks)
   print conn.get_usage()

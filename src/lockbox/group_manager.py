690b0cdfb952e9c58e6e5728008fc40a6131a8fc
Matt Tierney <tierney@cs.nyu.edu>
2011-09-26 09:16:27 -0400

    group man stuffs and doc removal for now

--- src/lockbox/group_manager.py
+++ src/lockbox/group_manager.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
 import gnupg
+import logging
+
+# logging.basicConfig(level=logging.DEBUG)
 
 class GroupManager(object):
   """Manages local knowledge of group membership."""
   def __init__(self):
     pass
 
+
 class Group(object):
   def __init__(self, gpg, owner, service_name_prefix):
+    """
+    Attributes:
+      service_name_prefix: group0_data domain => group0
+    """
     self.gpg = gpg
     self.owner = owner
     self.service_names_prefix = service_name_prefix
@@ -17,19 +25,31 @@ class Group(object):
     self.permissions = None
 
 
-  def __verify_username_key_already_imported(self, name):
-    pass
+  def _get_similar_already_imported_keys(self, name):
+    uid_to_fp = self.gpg.uid_to_fingerprint()
+    logging.debug(str(uid_to_fp))
+    candidate_keys = [uid_to_fp.get(uid) for uid in uid_to_fp
+                      if name.lower() in uid.lower()]
+    return candidate_keys
 
 
   def add_member(self, member):
-    self.members.append(member)
+    candidates = self._get_similar_already_imported_keys(member)
+    if not candidates:
+      return False
+
+    if len(candidates) > 1:
+      logging.warning('We do not account for multiple matching names yet. '
+                      'Taking first match.')
+    fingerprint = candidates[0]
+    print member, fingerprint
+    # self.members.append(member)
 
 
 def main():
   gpg = gnupg.GPG()
-  uid_to_fp = gpg.uid_to_fingerprint()
-  for uid in uid_to_fp:
-    print uid, uid_to_fp[uid]
+  group = Group(gpg, 'iamtheowner', 'group0')
+  group.add_member('matt tierney')
 
 if __name__=='__main__':
   main()

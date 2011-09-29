4c1140d1416f2112561a2a8ebaae411814e51bd7
Matt Tierney <tierney@cs.nyu.edu>
2011-09-29 16:12:24 -0400

    Whole-app testing (WIP).

--- tests/lockbox/test_lockbox.py
+++ tests/lockbox/test_lockbox.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
 
+from boto import connect_s3, connect_sdb, connect_sns, connect_sqs
 from lockbox import rsync
+from lockbox.gnupg import GPG
+from lockbox.event_handler import LockboxEventHandler
 import unittest
 
 
 class LockboxTestCase(unittest.TestCase):
   def setUp(self):
-    pass
+    self.s3_connection = connect_s3()
+    self.sdb_connection = connect_sdb()
+    self.sns_connection = connect_sns()
+    self.sqs_connection = connect_sqs()
+    self.gpg = GPG()
+    self.event_handler = LockboxEventHandler()
+
 
   def tearDown(self):
     pass
 
+
   def test_placeholder(self):
-    assert True == True
+    self.assertTrue(True)
+
 
 if __name__ == '__main__':
   unittest.main()

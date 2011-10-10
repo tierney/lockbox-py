fe35b9a07c8772df047e3d41cc07773a530d222b
Matt Tierney <tierney@cs.nyu.edu>
2011-10-10 12:02:01 -0400

    script to convient location

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-python main.py --lock_domain_name 'lock_domain' \
+python ../build/lib.linux-x86_64-2.7/lockbox/main.py \
+		--lock_domain_name 'lock_domain' \
 		--data_domain_name 'data_domain' \
 		--blob_bucket_name 'safe-deposit-box' \
 		--directory '~/lockbox'

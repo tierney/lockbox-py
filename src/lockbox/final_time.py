a502326910980f57960a00ec3cb98434678d8f02
Matt Tierney <tierney@cs.nyu.edu>
2011-10-19 12:09:53 -0400

    Adding final time base code.

---
+++ src/lockbox/final_time.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+class FinalTime(object):
+  """Removes any evidence of Lockbox management data, including local state
+  databases, deletes values stored in the cloud, putting the system into a state
+  where first_time.py could be run without conflict."""
+  
+  # Determine which groups we are the owner of so that we can shutdown the
+  # system.
+  pass
